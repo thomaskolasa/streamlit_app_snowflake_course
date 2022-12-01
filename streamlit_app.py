9ba5858d5c868e62435c23d386e6b3e4ff4a371a
thomaskolasa <11896498+thomaskolasa@users.noreply.github.com>
2022-11-30 22:17:10 -0500

    Update streamlit_app.py
    Change subtitle and button text

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,13 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our fruit list - Add your favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * FROM fruit_load_list")
         return my_cur.fetchall()
 
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get fruit list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
