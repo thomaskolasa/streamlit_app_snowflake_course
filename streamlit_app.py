34a2a76344ffef1b2a40e73363bbfbb10197869f
thomaskolasa <11896498+thomaskolasa@users.noreply.github.com>
2022-11-30 21:40:33 -0500

    Update streamlit_app.py
    Add stop command for error checking

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
